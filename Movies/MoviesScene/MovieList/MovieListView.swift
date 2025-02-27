ebb1d725bec4fb9dfa57a7e3b22fc3dcd69a2b5a
Rhytthm <rhytthm1717@gmail.com>
2025-02-27 13:24:00 +0530

    adding image view

--- Movies/MoviesScene/MovieList/MovieListView.swift
+++ Movies/MoviesScene/MovieList/MovieListView.swift
@@ -49,14 +49,46 @@ class MoviewListVC: UIViewController, UITableViewDelegate, UITableViewDataSource
         
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         movieListVM.movies?.results.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        
+        // Setting text
         cell.textLabel?.text = movieListVM.movies?.results[indexPath.row].title
+        
+        // Loading image
+        if let url = URL(string: MovieListEndpoints.image(path: movieListVM.movies?.results[indexPath.row].poster ?? "").imgUrl) {
+            loadImage(from: url) { image in
+                DispatchQueue.main.async {
+                    cell.imageView?.image = image
+                    cell.imageView?.isHidden = false
+                    cell.setNeedsLayout()  // Force the cell to re-layout
+                }
+            }
+        } else {
+            cell.imageView?.isHidden = true  // Hide imageView if URL is nil
+        }
+
+        // Return the cell
         return cell
+    }
+
+    // Assuming your loadImage function is asynchronous and uses a completion handler
+    func loadImage(from url: URL, completion: @escaping (UIImage?) -> Void) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data, let image = UIImage(data: data) {
+                completion(image)
+            } else {
+                completion(nil)
+            }
+        }.resume()
     }
 }
 
